17c6159329f2f76862bc820073ef61e43b5c3dc4
juanalvaradofo-art <juan.alvaradofo@gmail.com>
2026-06-25 00:01:11 -0500

    Updated next.config.mjs

---
+++ out/_next/static/chunks/app/legal/habeas-data/page-3155a61c307d601c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[374,544,832,894,927],{4492:(e,s,n)=>{Promise.resolve().then(n.bind(n,5958)),Promise.resolve().then(n.t.bind(n,6874,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[759,441,684,358],()=>s(4492)),_N_E=e.O()}]);
